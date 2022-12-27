3252eb42eccace5e2241253a711fbc37907def71
mikedr <derosamiguel@gmail.com>
2022-12-27 00:00:37 -0300

    Postfix notation

--- scalaMethodNotation/src/main/scala/MethodNotations.scala
+++ scalaMethodNotation/src/main/scala/MethodNotations.scala
@@ -1,12 +1,21 @@
+import scala.language.postfixOps
+
 object MethodNotations extends App {
 
   val miguel = new Person("Miguel", "The Godfather")
   println(miguel likes "The Godfather") //infix notation - operator notation
   val gabriela = new Person("Gabriela", "Back to the future")
   println(miguel hangOutWith gabriela)
+
+  println(!miguel) //tipo de invoación 1
+  println(gabriela.unary_!) //tipo de invoación 2
+
+  println(miguel isAlive)
 }
 
 class Person(val name: String, favoriteMovie: String) {
   def likes(movie: String): Boolean = movie == favoriteMovie
   def hangOutWith(otherPerson: Person): String = s"$name hang outs with ${otherPerson.name}"
+  def unary_! : String = s"$name unary operator!"
+  def isAlive: Boolean = true
 }
