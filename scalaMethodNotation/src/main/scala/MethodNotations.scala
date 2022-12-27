2fc114be3ade19160764a4c51aaa94f7fe258f0f
mikedr <derosamiguel@gmail.com>
2022-12-27 16:20:19 -0300

    Overload de método operador

--- scalaMethodNotation/src/main/scala/MethodNotations.scala
+++ scalaMethodNotation/src/main/scala/MethodNotations.scala
@@ -5,7 +5,6 @@ object MethodNotations extends App {
   val miguel = new Person("Miguel", "The Godfather")
   println(miguel likes "The Godfather") //infix notation - operator notation
   val gabriela = new Person("Gabriela", "Back to the future")
-  println(miguel + gabriela)
 
   println(!miguel) //tipo de invoación 1
   println(gabriela.unary_!) //tipo de invoación 2
@@ -14,12 +13,18 @@ object MethodNotations extends App {
 
   println(gabriela.apply()) //Forma 1 de llamar a apply, la común
   println(gabriela()) //Forma 2 de llamar a apply. Mágica
+
+  println(miguel + "El asador")
+  println(miguel + gabriela)
 }
 
 class Person(val name: String, favoriteMovie: String) {
   def likes(movie: String): Boolean = movie == favoriteMovie
   def +(otherPerson: Person): String = s"$name hang outs with ${otherPerson.name}"
+  def +(nickname: String): Person = new Person(s"$name ($nickname)",favoriteMovie)
   def unary_! : String = s"$name unary operator!"
   def isAlive: Boolean = true
   def apply(): String = s"Soy $name y mi pelicula favorita es $favoriteMovie"
+
+  override def toString = s"$name"
 }
