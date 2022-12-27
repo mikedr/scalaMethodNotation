e5176db6b20a6b0ba3f2efcca9c52593675f87d2
mikedr <derosamiguel@gmail.com>
2022-12-27 15:54:59 -0300

    Metodo nombrado con el operador +

--- scalaMethodNotation/src/main/scala/MethodNotations.scala
+++ scalaMethodNotation/src/main/scala/MethodNotations.scala
@@ -5,7 +5,7 @@ object MethodNotations extends App {
   val miguel = new Person("Miguel", "The Godfather")
   println(miguel likes "The Godfather") //infix notation - operator notation
   val gabriela = new Person("Gabriela", "Back to the future")
-  println(miguel hangOutWith gabriela)
+  println(miguel + gabriela)
 
   println(!miguel) //tipo de invoación 1
   println(gabriela.unary_!) //tipo de invoación 2
@@ -18,7 +18,7 @@ object MethodNotations extends App {
 
 class Person(val name: String, favoriteMovie: String) {
   def likes(movie: String): Boolean = movie == favoriteMovie
-  def hangOutWith(otherPerson: Person): String = s"$name hang outs with ${otherPerson.name}"
+  def +(otherPerson: Person): String = s"$name hang outs with ${otherPerson.name}"
   def unary_! : String = s"$name unary operator!"
   def isAlive: Boolean = true
   def apply(): String = s"Soy $name y mi pelicula favorita es $favoriteMovie"
