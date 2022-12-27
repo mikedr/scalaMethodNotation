9b605a173c3cc42e45c8598378293c28dbfad717
mikedr <derosamiguel@gmail.com>
2022-12-27 16:36:08 -0300

    Operador unario

--- scalaMethodNotation/src/main/scala/MethodNotations.scala
+++ scalaMethodNotation/src/main/scala/MethodNotations.scala
@@ -16,9 +16,13 @@ object MethodNotations extends App {
 
   println(miguel + "El asador")
   println(miguel + gabriela)
+
+  def nuevaPersona = new Person("Bruce", "Batman",50)
+  println(+nuevaPersona)
 }
 
-class Person(val name: String, favoriteMovie: String) {
+class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
+
   def likes(movie: String): Boolean = movie == favoriteMovie
   def +(otherPerson: Person): String = s"$name hang outs with ${otherPerson.name}"
   def +(nickname: String): Person = new Person(s"$name ($nickname)",favoriteMovie)
@@ -26,5 +30,6 @@ class Person(val name: String, favoriteMovie: String) {
   def isAlive: Boolean = true
   def apply(): String = s"Soy $name y mi pelicula favorita es $favoriteMovie"
 
-  override def toString = s"$name"
+  override def toString = s"$name ($age años)"
+  def unary_+(): Person = new Person(name, favoriteMovie, age+1)
 }
