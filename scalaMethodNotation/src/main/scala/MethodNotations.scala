82c453533f4e969ef30ebfc9f9f9fde86eefb27e
mikedr <derosamiguel@gmail.com>
2022-12-26 23:16:53 -0300

    Se agrega método

--- scalaMethodNotation/src/main/scala/MethodNotations.scala
+++ scalaMethodNotation/src/main/scala/MethodNotations.scala
@@ -6,4 +6,5 @@ object MethodNotations extends App {
 
 class Person(val name: String, favoriteMovie: String) {
   def likes(movie: String): Boolean = movie == favoriteMovie
+  def hangOutWith(otherPerson: String): String = s"$name hang outs with $otherPerson"
 }
